0c266e3a810b40104f7d237dceca719ef78763d4
meltrust <original.mtapia@outlook.com>
2021-04-14 17:26:39 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'events#index'
+
   resources :events, only: %i[new create index]
+  resources :users, only: %i[new create index]
 end
