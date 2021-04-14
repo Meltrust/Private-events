708f67eaea6d2b647f861c13b044b80dfb8fb04d
smy5152 <shabbiryamani@gmail.com>
2021-04-15 01:00:44 +0200

    DEVISE: Generated views & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'events#index'
-
   resources :events, only: %i[new create index]
   resources :users, only: %i[new create index]
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
 end
