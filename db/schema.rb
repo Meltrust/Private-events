9e1a677792883d735562db6ce4687ad96c29aa23
smy5152 <shabbiryamani@gmail.com>
2021-04-17 21:09:06 +0200

    MODEL: Enrollments & Associations with User & Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_161113) do
+ActiveRecord::Schema.define(version: 2021_04_17_190357) do
+
+  create_table "enrollments", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
