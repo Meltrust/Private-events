f7bcf1dc8e21bb556e13ed0a854e91ba7436ba58
smy5152 <shabbiryamani@gmail.com>
2021-04-18 23:25:55 +0200

    RUBOCOP: .rb files corrected

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,11 +1,11 @@
 class EnrollmentsController < ApplicationController
-#   before_action :require_login
-  
+  #   before_action :require_login
+
   def create
     event = Event.find(params[:event_id])
     enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
     if enrollment.save
-      flash[:notice] = "Enrolled Successfully!"
+      flash[:notice] = 'Enrolled Successfully!'
       redirect_to events_path(event_id: event.id)
     else
       flash[:notice] = 'Ooops! Something went wrong...'
