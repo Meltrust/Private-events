f315325570dc131a2a75da318a5c6cd1dd57c473
smy5152 <shabbiryamani@gmail.com>
2021-04-19 01:31:14 +0200

    ENROLL: Blocked multiple enrollments for already enrolled

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,15 +1,18 @@
 class EnrollmentsController < ApplicationController
-  #   before_action :require_login
-
   def create
     event = Event.find(params[:event_id])
-    enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
-    if enrollment.save
-      flash[:notice] = 'Enrolled Successfully!'
-      redirect_to events_path(event_id: event.id)
-    else
-      flash[:notice] = 'Ooops! Something went wrong...'
+    if (Enrollment.where(event_id: event.id, user_id: params[:user_id]).count)?
+      flash[:notice] = 'Already Enrolled For this Event!'
       redirect_to event_path(event)
+    else
+      enrollment = Enrollment.new(event_id: event.id, user_id: params[:user_id])
+      if enrollment.save
+        flash[:notice] = 'Enrolled Successfully!'
+        redirect_to events_path(event_id: event.id)
+      else
+        flash[:notice] = 'Ooops! Something went wrong...'
+        redirect_to event_path(event)
+      end
     end
   end
 end
