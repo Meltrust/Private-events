6eec6d331999d622ae10ebec10d3b171300807ba
smy5152 <shabbiryamani@gmail.com>
2021-04-20 03:10:41 +0200

    Rubocop & Linters

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,12 +3,14 @@ RSpec.describe Event, type: :model do
   user = User.create(name: 'user3')
   describe 'An event can be created by a user and if it has all the validations' do
     it 'belongs to a user' do
-      event = Event.create(user_id: user.id, name: 'event name', date: Date.today, location: 'some location', description: 'description of the event')
+      event = Event.create(user_id: user.id, name: 'event name', date: Date.today, location: 'some location',
+                           description: 'description of the event')
       expect(event.user_id).to eq(user.id)
     end
 
     it 'event is not valid ' do
-      event = Event.create(user_id: user.id, name: 'event name', date: Date.today, location: 'some location', description: 'description of the event')
+      event = Event.create(user_id: user.id, name: 'event name', date: Date.today, location: 'some location',
+                           description: 'description of the event')
 
       expect(event).to_not be_valid
     end
@@ -16,8 +18,10 @@ RSpec.describe Event, type: :model do
 
   it '2 events not valid without user:name & Eventdate|User_ID' do
     user = User.create
-      event = Event.create(name: 'event name', date: Date.today, location: 'some location', description: 'description of the event', user_id: nil)
-      event2 = Event.create(name: 'event name', date: nil, location: 'some location', description: 'description of the event', user_id: user.id)
+    event = Event.create(name: 'event name', date: Date.today, location: 'some location',
+                         description: 'description of the event', user_id: nil)
+    event2 = Event.create(name: 'event name', date: nil, location: 'some location',
+                          description: 'description of the event', user_id: user.id)
 
     expect(event).to_not be_valid
     expect(event2).to_not be_valid
@@ -28,11 +32,12 @@ RSpec.describe Event, type: :model do
     attendee1 = User.create(name: 'attendee1')
     attendee2 = User.create(name: 'attendee2')
 
-    event = Event.create(user_id: creator.id, name: 'event name', date: Date.today, location: 'some location', description: 'description of the event')
+    event = Event.create(user_id: creator.id, name: 'event name', date: Date.today, location: 'some location',
+                         description: 'description of the event')
     event.attendees = [attendee1, attendee2]
     event.save
 
     saved_event = Event.find(event.id)
-    expect(event.attendees.size).to eq(2)
+    expect(saved_event.attendees.size).to eq(2)
   end
 end
