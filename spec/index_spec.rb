f22bef7467c65fbbfe95a88393533ddb2fb79c68
smy5152 <shabbiryamani@gmail.com>
2021-04-20 17:27:54 +0200

    UPDATE: RSPEC & Capybara tests. done

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'index', type: :system do
   describe 'index' do
     it 'shows past events' do
       visit events_path
-      expect(page).to have_content('Past events')
+      expect(page).to have_content('Past Events')
     end
     it 'shows upcoming events' do
       visit events_path
-      expect(page).to have_content('Upcoming events')
+      expect(page).to have_content('Upcoming Events')
     end
   end
 end
