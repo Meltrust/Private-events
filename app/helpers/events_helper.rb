c7f613c5ff0ce3c25e7d102bcaedbd0e18826125
smy5152 <shabbiryamani@gmail.com>
2021-04-21 02:52:17 +0200

    HELPERS: Added and used helpers created

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,22 +3,22 @@ module EventsHelper
     @creator = User.find_by(id: user_id).name
   end
 
-  def attendees_list
-    list = " "
-    @event.attendees.each do |attendee|
-      list = list + attendee.name + ", "
+  def attendees_list(event_obj = nil)
+    list = ' '
+    event_obj ||= @event
+    event_obj.attendees.each do |attendee|
+      list = "#{list}#{attendee.name}, "
     end
     list
   end
 
   def enroll_button
-    if logged_in? && (@event.date >= Date.today)
-      return (button_to "Enroll", enrollment_path(event_id: @event.id, user_id: current_user.id), method: :post)
-    end
+    (return unless logged_in? && (@event.date >= Date.today))
+    button_to 'Enroll', enrollment_path(event_id: @event.id, user_id: current_user.id), method: :post
   end
 
   def enroll_upcoming_events(event)
-    logged_in? ? (button_to "Enroll", enrollment_path(event_id: event.id, user_id: current_user.id), method: :post) : ""
+    logged_in? ? (button_to 'Enroll', enrollment_path(event_id: event.id, user_id: current_user.id), method: :post) : ''
   end
 
   def new_event
@@ -31,17 +31,17 @@ module EventsHelper
 
   def logout_link
     (link_to 'Logout', logout_path) if logged_in?
-  end 
-  
+  end
+
   def new_user_link
     (link_to 'New User', new_user_path) unless logged_in?
   end
 
   def login_link
-    (link_to 'Login', login_path) unless logged_in? 
+    (link_to 'Login', login_path) unless logged_in?
   end
 
   def events_all
-    link_to 'Back to Events', root_path
+    link_to 'All Events', root_path
   end
 end
